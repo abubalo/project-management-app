601f4863b67539aa4300d93e0326fb4b62846a79
Abubakar Balogun <abzynobalogun@gmail.com>
2025-03-15 10:32:28 +0200

    Remove mongodb crediential

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
     appDir: true,
   },
 
-  evn:{
-    MONGODB_URL: "mongodb+srv://x-note:qa5mnZTa4AYDk0IO@cluster0.lquq3n1.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: "snh2jd92kjsmas",
-    S3_Bucket: "",
-    TWITTER_CLONE: "5YuSH05n8LaiuJJJ"
-  }
+
 }
 
 module.exports = nextConfig
